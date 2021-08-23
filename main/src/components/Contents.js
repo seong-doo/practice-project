06fbd37e82942bbeecb4dc479d1dceb70e201f09
JSD <tjden0305@gmail.com>
2021-08-23 23:25:50 +0900

    make main page

---
+++ main/src/components/Contents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contents = () => {
+    return (
+        <section>
+          <h2>Main page</h2>
+          <div className="contents">
+
+          </div>
+        </section>
+    )
+}
+
+export default Contents
---
+++ main/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className = "header">
+        <h1>Personal Project List</h1>
+        <select>
+          <option>project1</option>
+          <option>project2</option>
+        </select>
+      </header>
+    )
+}
+
+export default Header
